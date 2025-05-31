98f25b4ebd0115d43cd938841b112ecfdcccefaa
Syauqi-Sy <syauqi.sy926@gmail.com>
2025-05-31 20:39:22 +0700

    asdf

--- KumpulanInterface.java
+++ KumpulanInterface.java
@@ -1,29 +1,30 @@
 public interface KumpulanInterface {
-      interface ShippingCostCalculator {
-        int PRICE_PER_KG = 2000;
-    
-        public double calculateCost();
-    }
-    
-    interface MassConverter {
-        int DENOMINATOR = 1000;
-    
-        public double gramToKilogram();
-    }
-    
-    interface MassCalculable {
-        int DENSITY = 8;
-        double THICKNESS = 0.5;
-    
-        public double getMass();
-    }
-    
-    interface PiRequired {
-        double PI = 22.0 / 7.0;
-    }
-    
-    interface ThreeDimensional {
-        public double getSurfaceArea();
-        public double getVolume();
-    }
+}
+
+interface ShippingCostCalculator {
+    int PRICE_PER_KG = 2000;
+
+    public double calculateCost();
+}
+
+interface MassConverter {
+    int DENOMINATOR = 1000;
+
+    public double gramToKilogram();
+}
+
+interface MassCalculable {
+    int DENSITY = 8;
+    double THICKNESS = 0.5;
+
+    public double getMass();
+}
+
+interface PiRequired {
+    double PI = 22.0 / 7.0;
+}
+
+interface ThreeDimensional {
+    public double getSurfaceArea();
+    public double getVolume();
 }
